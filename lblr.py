e830b58b58d098fd00a90f480b6f4ca055427679
robsigler <robert.b.sigler.mn@gmail.com>
2019-11-03 00:27:48 -0500

    Embed jpg images in output HTML

--- lblr.py
+++ lblr.py
@@ -1,8 +1,11 @@
 from lxml import html
 from bs4 import BeautifulSoup
+from shutil import copyfile
+
+SRC_DIR = "C:/Users/rob/Google Drive/LBLR_Project/littlebass.com/"
 
 def lblr():
-    path_to_file = "C:/Users/rob/Google Drive/LBLR_Project/littlebass.com/note0apr.html"
+    path_to_file = "{}/note0apr.html".format(SRC_DIR)
     with open(path_to_file) as open_html_file:
         html_text = open_html_file.read()
     entire_page = BeautifulSoup(html_text, "html.parser")
@@ -31,7 +34,12 @@ def lblr():
         img_div = soup.new_tag("div")
         date_div.append(img_div)
         for link in links:
-            img_tag = soup.new_tag("img", href=link.get("href"))
+            link_href = link.get("href")
+            if "javascript:open_win" in link_href:
+                link_href = "{}.jpg".format(link_href.split("(")[1][:-1])
+                src_img = "{}/{}".format(SRC_DIR, link_href)
+                copyfile(src_img, link_href)
+            img_tag = soup.new_tag("img", src=link_href)
             img_div.append(img_tag)
         new_page_body.append(date_div)
     print(new_page_body.prettify())
